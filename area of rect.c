76a03800c1906f51a7f7a39f31f09bc17d37c35a
Jay9Dwivedi <105783060+Jay9Dwivedi@users.noreply.github.com>
2022-07-15 20:30:31 +0530

    Add files via upload
    practise set 1

---
+++ area of rect.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+main()
+{
+
+float A,l,b;
+printf ("please enter the l value:\n");
+scanf("%f",&l);
+
+printf("please enter the b value:\n");
+scanf("%f",&b);
+
+A=l*b;
+
+printf ("the area of rectangle is %f",A);
+
+	return 0;
+}
+
+
+
+
