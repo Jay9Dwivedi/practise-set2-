57687b0eb0be1ffc16c01c29e3a10b22d0fb2dfa
Jay9Dwivedi <105783060+Jay9Dwivedi@users.noreply.github.com>
2022-07-15 20:32:54 +0530

    Add files via upload
    area of circle in c

---
+++ circle.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+float pie=3.14,r,A;
+
+printf ("enter the radius:\n");
+scanf ("%f",&r);
+
+A= pie*r*r;
+
+printf ("the area of circle is: %f",A);
+	
+	return 0;
+}
